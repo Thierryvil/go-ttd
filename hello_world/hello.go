272e86162eb336dba3c4d5330b6002cc919f7a27
Thierry Tajes dos Santos <thierrytajesdossantos@gmail.com>
2023-07-10 21:35:48 -0300

    refact: add helper function to hello world tests

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -3,6 +3,9 @@ package main
 const prefixHelloPortuguese = "Olá, "
 
 func Hello(nome string) string {
+	if nome == "" {
+		nome = "Mundo!"
+	}
 	return prefixHelloPortuguese + nome
 }
 
--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHelloWorld(t *testing.T) {
-	resultado := Hello("Mundo!")
-	esperado := "Olá, Mundo!"
 
-	if resultado != esperado {
-		t.Errorf("Resultado '%s', esperado '%s'", resultado, esperado)
+	checkRightAnswer := func(t *testing.T, result, expected string) {
+		t.Helper()
+		if result != expected {
+			t.Errorf("resultado: '%s', esperado: '%s'", result, expected)
+
+		}
 	}
+	t.Run("Deve retornar: Olá, Chris", func(t *testing.T) {
+		result := Hello("Chris!")
+		expected := "Olá, Chris!"
+		checkRightAnswer(t, result, expected)
+
+		t.Run("Deve retornar Olá, Mundo!", func(t *testing.T) {
+			result := Hello("Mundo!")
+			expected := "Olá, Mundo!"
+
+			checkRightAnswer(t, result, expected)
+		})
+	})
 }
